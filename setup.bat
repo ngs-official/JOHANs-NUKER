42e6765a3b0ec7dd20112768dcd781629472531b
J0HAN <123272327+ngs-official@users.noreply.github.com>
2023-08-21 22:18:18 -0400

    Added more stuff

--- setup.bat
+++ setup.bat
@@ -3,7 +3,9 @@ title JOHAN's NUKER
 echo Downloading all the nessecary Python libraries...
 echo.
 pip install -U discord.py
+py -3 -m pip install -U discord.py
 pip install colorama
+py -3 -m pip install colorama
 echo.
 echo Completed! Run the 'start.bat' file to start up the bot!
 echo.
